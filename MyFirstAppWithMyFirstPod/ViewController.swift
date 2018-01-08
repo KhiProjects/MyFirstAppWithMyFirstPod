690a9fd0b30348ba453a1c48a7222b3d9a558807
kevin <kevin.hijlkema@meteoconsult.com>
2018-01-08 18:02:11 +0100

    MFAWMFP: add pod in project

--- MyFirstAppWithMyFirstPod/ViewController.swift
+++ MyFirstAppWithMyFirstPod/ViewController.swift
@@ -7,19 +7,49 @@
 //
 
 import UIKit
+import FirstTestPod
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MyKitProtocol {
 
+    let pod = MyKit.shared
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        pod.open("1234", withNotifier: self)
+        pod.doSomething("do some stuff")
+        
+        let gesture = UITapGestureRecognizer(target: self, action: #selector(ViewController.didClickOnView))
+        gesture.numberOfTapsRequired = 1
+        gesture.numberOfTouchesRequired = 1
+        self.view.addGestureRecognizer(gesture)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        didClickOnView()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    //Methodes
+    func didClickOnView() {
+        self.view.backgroundColor = pod.getAColor()
+    }
 
 
+    //MYKIT DELEGATE
+    func didAskToDoSomething() {
+        NSLog("HEYYY Mykkit ask for it !")
+    }
+    
+    func configurationMCKit() -> MyKitConfig {
+        return MyKitConfig(alpha: 0.5, key: "1234")
+    }
 }
 
